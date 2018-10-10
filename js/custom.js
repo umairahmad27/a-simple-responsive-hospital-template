4485af85b8a7cc7d9095fb0f195686c33b6c728d
Umair Ahmad <m.umairahmad1@gmail.com>
2018-10-10 22:32:29 +0500

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+// Date & Time
+setInterval(function () {
+    var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+    document.getElementById('time').innerHTML = now;
+}, 1000)
+
+// Smooth Scroll
+$(document).ready(function () {
+
+    $(".scrollLink").on('click', function (event) {
+
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 1000);
+
+    });
+})
+
+// Google Map
+function initMap() {
+    // The location of Hospital
+    var hospitalLocation = { lat: 31.4432, lng: 73.0989 };
+    // The map, centered at Hospital
+    var map = new google.maps.Map(
+        document.getElementById('map'),{ zoom: 16, center: hospitalLocation });
+    // The marker, positioned at Hospital
+    var marker = new google.maps.Marker({ position: hospitalLocation, map: map });
+}
+initMap();
---
+++ js/plugins.js
@@ -0,0 +1,15 @@
+// Side Nav
+var elems = document.querySelectorAll('.sidenav');
+var instances = M.Sidenav.init(elems);
+
+// Slider
+var elems = document.querySelectorAll('.slider');
+var instances = M.Slider.init(elems);
+
+// Tool Tip
+var elems = document.querySelectorAll('.tooltipped');
+var instances = M.Tooltip.init(elems);
+
+// Fixed Action Button
+var elems = document.querySelectorAll('.fixed-action-btn');
+var instances = M.FloatingActionButton.init(elems);
